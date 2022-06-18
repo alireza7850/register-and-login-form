c2ed391496d24d024f78368b3c28f9190192e9b9
alireza <alireza5025@yahoo.com>
2022-06-18 15:16:11 +0430

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".register-btn").click(function () {
+    $("#container").addClass("alt");
+  });
+});
+$(document).ready(function () {
+  $(".login-btn").click(function () {
+    $("#container").removeClass("alt");
+  });
+});
